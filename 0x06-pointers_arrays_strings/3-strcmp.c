90cad735d2ea75928c681d84430ec1f8c3896a79
Mohamed Abdi <mosolves147@gmail.com>
2022-03-23 21:45:30 +0300

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,26 @@
 #include "main.h"
 /**
- *_strcmp - compares two strings
- *
- *@s1:string 1
- *@s2:string 2
- *
- *Return:0- same,-ve - less ,+ve -more
- */
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if matching, and ns1 - ns2 if not matching
+*/
+
 int _strcmp(char *s1, char *s2)
 {
-while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+int i;
+
+/**
+ * We have reached the end of S1 OR we have reached the end of S2
+ * 
+*/
+
+for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 {
-s1++;
-s2++;
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
 }
-if (*s1 == *s2)
-{
+}
 return (0);
-}
-else
-{
-return (*s1 - *s2);
-}
 }
