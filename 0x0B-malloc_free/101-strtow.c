ba5cba1f7ef537efd485a9540ddc18db6cd9a446
Mohamed Abdi <mosolves147@gmail.com>
2022-04-04 21:16:17 +0300

    Split a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,22 +1,18 @@
 #include <stdlib.h>
-
 /**
  * strtow - char
  * @str: pointer to string params
  * Return: char
  */
-
 char **strtow(char *str)
 {
 int i = 0, j = 0, k = 0;
 int len = 0, count = 0;
 char **f, *col;
-
 if (!str || !*str)
 {
 return (NULL);
 }
-
 while (*(str + i))
 {
 if (*(str + i) != ' ')
@@ -28,20 +24,17 @@ count += 1;
 }
 i++;
 }
-
 if (count == 0)
 {
 return (NULL);
 }
 count += 1;
 f = malloc(sizeof(char *) * count);
-
 if (!f)
 {
 return (NULL);
 }
 i = 0;
-
 while (*str)
 {
 while (*str == ' ' && *str)
@@ -49,14 +42,12 @@ while (*str == ' ' && *str)
 str++;
 }
 len = 0;
-
 while (*(str + len) != ' ' && *(str + len))
 {
 len += 1;
 }
 len += 1;
 col = malloc(sizeof(char) * len);
-
 if (!col)
 {
 for (k = j - 1; k >= 0; k--)
@@ -66,14 +57,12 @@ free(f[k]);
 free(f);
 return (NULL);
 }
-
 for (k = 0; k < (len - 1);  k++)
 {
 *(col + k) = *(str++);
 }
 *(col + k) = '\0';
 *(f + j) = col;
-
 if (j < (count - 1))
 {
 j++;
