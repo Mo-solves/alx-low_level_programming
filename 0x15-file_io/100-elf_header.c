a12d595dcce5c928912650a13a85dacc0dda443a
Mohamed Abdi <mosolves147@gmail.com>
2022-05-06 21:18:20 +0300

    betty fix

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -16,7 +16,6 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
-
 /**
  * check_elf - Checks if a file is an ELF file.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
@@ -26,7 +25,6 @@ void close_elf(int elf);
 void check_elf(unsigned char *e_ident)
 {
 int index;
-
 for (index = 0; index < 4; index++)
 {
 if (e_ident[index] != 127 &&
@@ -49,13 +47,10 @@ exit(98);
 void print_magic(unsigned char *e_ident)
 {
 int index;
-
 printf("  Magic:   ");
-
 for (index = 0; index < EI_NIDENT; index++)
 {
 printf("%02x", e_ident[index]);
-
 if (index == EI_NIDENT - 1)
 printf("\n");
 else
@@ -70,7 +65,6 @@ printf(" ");
 void print_class(unsigned char *e_ident)
 {
 printf("  Class:                             ");
-
 switch (e_ident[EI_CLASS])
 {
 case ELFCLASSNONE:
@@ -94,7 +88,6 @@ printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 void print_data(unsigned char *e_ident)
 {
 printf("  Data:                              ");
-
 switch (e_ident[EI_DATA])
 {
 case ELFDATANONE:
@@ -119,7 +112,6 @@ void print_version(unsigned char *e_ident)
 {
 printf("  Version:                           %d",
 e_ident[EI_VERSION]);
-
 switch (e_ident[EI_VERSION])
 {
 case EV_CURRENT:
@@ -138,7 +130,6 @@ break;
 void print_osabi(unsigned char *e_ident)
 {
 printf("  OS/ABI:                            ");
-
 switch (e_ident[EI_OSABI])
 {
 case ELFOSABI_NONE:
@@ -195,9 +186,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 {
 if (e_ident[EI_DATA] == ELFDATA2MSB)
 e_type >>= 8;
-
 printf("  Type:                              ");
-
 switch (e_type)
 {
 case ET_NONE:
@@ -228,17 +217,14 @@ printf("<unknown: %x>\n", e_type);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
 printf("  Entry point address:               ");
-
 if (e_ident[EI_DATA] == ELFDATA2MSB)
 {
 e_entry = ((e_entry << 8) & 0xFF00FF00) |
 ((e_entry >> 8) & 0xFF00FF);
 e_entry = (e_entry << 16) | (e_entry >> 16);
 }
-
 if (e_ident[EI_CLASS] == ELFCLASS32)
 printf("%#x\n", (unsigned int)e_entry);
-
 else
 printf("%#lx\n", e_entry);
 }
@@ -273,7 +259,6 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 Elf64_Ehdr *header;
 int o, r;
-
 o = open(argv[1], O_RDONLY);
 if (o == -1)
 {
@@ -295,7 +280,6 @@ close_elf(o);
 dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 exit(98);
 }
-
 check_elf(header->e_ident);
 printf("ELF Header:\n");
 print_magic(header->e_ident);
@@ -306,7 +290,6 @@ print_osabi(header->e_ident);
 print_abi(header->e_ident);
 print_type(header->e_type, header->e_ident);
 print_entry(header->e_entry, header->e_ident);
-
 free(header);
 close_elf(o);
 return (0);
